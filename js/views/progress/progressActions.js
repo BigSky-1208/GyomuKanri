4568f958973bbeb39e9ec30a0301c03449718a4c
BigSky-1208 <sora.yamashita@neo-career.co.jp>
2025-12-23 10:16:30 +0900

    Update progressActions.js

--- js/views/progress/progressActions.js
+++ js/views/progress/progressActions.js
@@ -2,7 +2,7 @@
 
 import { db, allTaskObjects } from "../../main.js";
 import { doc, updateDoc, Timestamp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
-import { showConfirmationModal, hideConfirmationModal } from "../../components/modal.js";
+import { showConfirmationModal, hideConfirmationModal } from "../../components/modal/index.js";
 import { escapeHtml } from "../../utils.js";
 
 export async function handleCompleteGoal(taskName, goalId, onSuccessCallback) {
